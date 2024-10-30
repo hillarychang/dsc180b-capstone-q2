3e17b485d596147b744732b82d9deebcff4d2522
kev374k <kev374k@gmail.com>
2024-10-30 10:25:17 -0700

    added script functionality

--- README.md
+++ README.md
@@ -41,12 +41,15 @@ This project implements a data pipeline for Quarter 1 of our DSC Capstone. For t
 │   ├──feature_params.json
 │   └──model_params.json
 │
+├── scripts                          <- contains scripts for ease of use
+│   │──text_cleaner.py               <- used to clean the 'memo' column
 │
 ├── notebooks                         <- jupyter notebooks used for data analysis
 │   ├──hillary_data_exploration.ipynb <- Hillary Chang's Data Analysis Notebook         
 │   ├──kevin_data_exploration.ipynb   <- Kevin Wong's Data Analysis Notebook
 │   ├──kurumi_data_exploration.ipynb  <- Kurumi Kaneko's Data Analysis Notebook
-│   ├──jevan_week2.ipynb              <- Jevan Chahal's notebook for train/test split
+│   └──jevan_week2.ipynb              <- Jevan Chahal's notebook for train/test/split
+│
 ├── res  
 │   └──predicted_result.csv     <- predicted model results
 │
--- run.py
+++ run.py
@@ -1,12 +1,34 @@
 import sys
 import json
+import argparse
+import pandas as pd
 from src.etl import get_data
 from src.features import build_features
 from src.model_training import train_model
+from src.text_cleaner import clean_text
+
+def clean_data(data):
+    """
+    Cleans the 'memo' column of the data
+    """
+    cleaned_df = clean_text(data)
+    return cleaned_df
 
 def main(targets):
     pass
 
 if __name__ == '__main__':
-    targets = sys.argv[1:]
-    main(targets)
+    # Create Argument Parser in order to read in targets
+    parser = argparse.ArgumentParser(description = "Used to Run Model")
+    parser.add_argument('dataset', choices = ['inflows', 'outflows'], help = "Specify the Dataset that should be Cleaned")
+    args = parser.parse_args()
+
+    # Determine which Dataset should be cleaned
+    if args.dataset == 'inflows':
+        file_name = "../data/ucsd-inflows.pqt"
+    else:
+        file_name = "../data/ucsd-outflows.pqt"
+    
+    # Read in and clean the data
+    data = pd.read_parquet(file_name)
+    cleaned_data = clean_data(data)

539901860aa55fb3f34283b6d14d73b77bb091ff
JevanC <128771585+JevanC@users.noreply.github.com>
2024-10-30 11:09:19 -0700

    added add_token function

--- src/features.py
+++ src/features.py
@@ -1,2 +1,25 @@
 def build_features():
     pass
+    
+def add_token(data):
+    def add_text(memo, added_tokens):
+        memo += ''.join(added_tokens)
+        return memo
+
+    def whole_dollar_amount(amount):
+        if amount % 1 == 0:
+            return ' <W_D>'
+        return ''
+
+    def day(date):
+        return f" <D_{date.day}>"
+
+    def month(date):
+        return f" <M_{date.month}>"
+
+    data['memo'] = data.apply(
+        lambda row: add_text(row['memo'], 
+                             [whole_dollar_amount(row['amount']), 
+                              day(row['posted_date']),
+                             month(row['posted_date'])]), axis=1
+    )

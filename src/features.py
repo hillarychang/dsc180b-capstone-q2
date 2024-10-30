9a791bde6c3fd5603b4a9de1585c1ea468624691
kev374k <kev374k@gmail.com>
2024-10-30 11:35:32 -0700

    updated README.md

--- src/features.py
+++ src/features.py
@@ -2,6 +2,8 @@ def build_features():
     pass
     
 def add_token(data):
+    df = data.copy()
+
     def add_text(memo, added_tokens):
         memo += ''.join(added_tokens)
         return memo
@@ -17,9 +19,11 @@ def add_token(data):
     def month(date):
         return f" <M_{date.month}>"
 
-    data['memo'] = data.apply(
+    df['memo'] = df.apply(
         lambda row: add_text(row['memo'], 
                              [whole_dollar_amount(row['amount']), 
                               day(row['posted_date']),
                              month(row['posted_date'])]), axis=1
     )
+
+    return df
